40732b3a4eb9ec18260a4920d7cd0008271c0cb0
Adam Saltz <saltz.adam@gmail.com>
2020-06-11 23:57:21 -0400

    switch to dash-leaflet

--- app/app.py
+++ app/app.py
@@ -12,6 +12,8 @@ from dash.dependencies import Input, Output, State
 import plotly.graph_objects as go
 import plotly_express as px
 
+import dash_leaflet as dl
+
 import folium
 import osmnx as ox
 
@@ -21,7 +23,6 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 with open("brighton_graph.pkl", 'rb') as pklfile:
     brighton_G = pkl.load(pklfile)
 
-# edges = brighton_G.edges(data=True)
 
 Safe = px.colors.qualitative.Safe
 angle_color_map = {
@@ -45,6 +46,8 @@ angle_color_map = {
 
 # COLORS = [Safe[3], Safe[6], Safe[2], Safe[1], Safe[9]]
 
+edge_getter = brighton_G.edges
+
 import geocoder
 def get_route(ori_str, des_str):
     g1 = geocoder.osm(ori_str + " Brighton, MA")
@@ -53,147 +56,18 @@ def get_route(ori_str, des_str):
     p2 = (g2.json['lng'], g2.json['lat'])
     n1 = ox.get_nearest_node(brighton_G,p1, method='haversine')
     n2 = ox.get_nearest_node(brighton_G,p2, method='haversine')
-    route = nx.shortest_path(brighton_G, n1, n2)
+    route = nx.shortest_path(brighton_G, n1, n2, weight='length_m')
     return route
 
-def _make_folium_polyline(
-    edge, edge_color, edge_width, edge_opacity, popup_attribute=None, **kwargs
-):
-    """
-    Turn row GeoDataFrame into a folium PolyLine with attributes.
-    Parameters
-    ----------
-    edge : GeoSeries
-        a row from the gdf_edges GeoDataFrame
-    edge_color : string
-        color of the edge lines
-    edge_width : numeric
-        width of the edge lines
-    edge_opacity : numeric
-        opacity of the edge lines
-    popup_attribute : string
-        edge attribute to display in a pop-up when an edge is clicked, if
-        None, no popup
-    kwargs : dict
-        Extra parameters passed through to folium
-    Returns
-    -------
-    pl : folium.PolyLine
-    """
-    # check if we were able to import folium successfully
-    if not folium:
-        raise ImportError("The folium package must be installed to use this optional feature.")
 
-    # locations is a list of points for the polyline
-    # folium takes coords in lat,lon but geopandas provides them in lon,lat
-    # so we have to flip them around
-    locations = list([(lat, lng) for lng, lat in edge["geometry"].coords])
-
-    # if popup_attribute is None, then create no pop-up
-    if popup_attribute is None:
-        popup = None
-    else:
-        # folium doesn't interpret html in the html argument (weird), so can't
-        # do newlines without an iframe
-        popup_text = json.dumps(edge[popup_attribute])
-        popup = folium.Popup(html=popup_text)
-
-    # create a folium polyline with attributes
-    pl = folium.PolyLine(
-        locations=locations,
-        popup=popup,
-        color=edge_color,
-        weight=edge_width,
-        opacity=edge_opacity,
-        **kwargs,
-    )
-    return pl
-
-
-
-def plot_route_folium_colored(
-    G,
-    route,
-    route_map=None,
-    popup_attribute=None,
-    tiles="cartodbpositron",
-    zoom=1,
-    fit_bounds=True,
-    color_fn=(lambda x : "#cc0000"),
-    route_width=5,
-    route_opacity=1,
-    **kwargs,
-):
-    """
-    Plot a route on an interactive folium web map.
-    Parameters
-    ----------
-    G : networkx.MultiDiGraph
-        input graph
-    route : list
-        the route as a list of nodes
-    route_map : folium.folium.Map
-        if not None, plot the route on this preexisting folium map object
-    popup_attribute : string
-        edge attribute to display in a pop-up when an edge is clicked
-    tiles : string
-        name of a folium tileset
-    zoom : int
-        initial zoom level for the map
-    fit_bounds : bool
-        if True, fit the map to the boundaries of the route's edges
-    route_color : string
-        color of the route's line
-    route_width : numeric
-        width of the route's line
-    route_opacity : numeric
-        opacity of the route lines
-    kwargs : dict
-        Extra parameters passed through to folium
-    Returns
-    -------
-    route_map : folium.folium.Map
-    """
-    # check if we were able to import folium successfully
-    if not folium:
-        raise ImportError("The folium package must be installed to use this optional feature.")
-
-    # create gdf of the route edges
-    gdf_edges = ox.utils_graph.graph_to_gdfs(G, nodes=False, fill_edge_geometry=True)
-    route_nodes = list(zip(route[:-1], route[1:]))
-    index = [
-        gdf_edges[(gdf_edges["u"] == u) & (gdf_edges["v"] == v)].index[0] for u, v in route_nodes
-    ]
-    gdf_route_edges = gdf_edges.loc[index]
-
-    # get route centroid
-    x, y = gdf_route_edges.unary_union.centroid.xy
-    route_centroid = (y[0], x[0])
-
-    # create the folium web map if one wasn't passed-in
-    if route_map is None:
-        route_map = folium.Map(location=route_centroid, zoom_start=zoom, tiles=tiles)
-
-    # add each route edge to the map
-    for _, row in gdf_route_edges.iterrows():
-        pl = _make_folium_polyline(
-            edge=row,
-            edge_color=color_fn(row),
-            edge_width=route_width,
-            edge_opacity=route_opacity,
-            popup_attribute=popup_attribute,
-            **kwargs,
-        )
-        pl.add_to(route_map)
-
-    # if fit_bounds is True, fit the map to the bounds of the route by passing
-    # list of lat-lng points as [southwest, northeast]
-    if fit_bounds and isinstance(route_map, folium.Map):
-        tb = gdf_route_edges.total_bounds
-        bounds = [(tb[1], tb[0]), (tb[3], tb[2])]
-        route_map.fit_bounds(bounds)
-
-    return route_map
+def make_line(u,v):
+    print(edge_getter[u,v,0]['geometry'])
+    geom = edge_getter[u,v,0]['geometry']
+    p0 = geom.coords[0]
+    p1 = geom.coords[-1]
+    color = angle_color_map[edge_getter[u,v,0]['angle_class']]
+    print(p0, p1, color)
+    return dl.Polyline(positions=[[p0[1], p0[0]], [p1[1], p1[0]]], color=color)
 
 def get_edge_color(row):
     return str(angle_color_map[row['angle_class']])
@@ -201,12 +75,10 @@ def get_edge_color(row):
 def get_fig(ori_str, des_str):
     print('get route')
     route = get_route(ori_str, des_str)
-    print('make map')
-    the_map = plot_route_folium_colored(brighton_G, route, color_fn = get_edge_color,route_width = 2)
-    print('save and load')
-    the_map.save('assets/the_map.html')
-    # return app.get_asset_url('the_map.html')
-    return open('assets/the_map.html', 'r').read()
+    route_nodes = list(zip(route[:-1], route[1:]))
+    lines = [make_line(u,v) for (u,v) in route_nodes]
+    print('drew',len(lines),'lines')
+    return lines
 
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -232,7 +104,8 @@ app.layout = html.Div([
     # dcc.Graph(id='the_graph')
     # html.Div(id='the_graph', children="")
     # dcc.Graph(id='the map', ),
-    html.Iframe(id='the map', width='100%', height='400'),
+    # html.Iframe(id='the map', width='100%', height='400'),
+    html.Div([dl.Map([dl.TileLayer(), dl.LayerGroup(id='layer')], style={'width': '1000px', 'height': '500px'}, center=[42.3493223,-71.1562415], zoom=15, id="the_map")]),
     html.Div(id='blurs', style={'display': 'none'})
 ])
 @app.callback(
@@ -257,7 +130,7 @@ def update_blurs(blur_o, blur_d):
         return int(blur_o) + int(blur_d)
 
 @app.callback(
-    Output('the map', 'srcDoc'),
+    Output('layer', 'children'),
     [Input('blurs', 'children')],# Input('dest', 'value')],
     [State('origin', 'value'),
      State('dest', 'value')]
@@ -269,9 +142,9 @@ def update_blurs(blur_o, blur_d):
 #             return("You're trying to go from " + ori_str + " to " + dest_str + ". The route uses nodes: " + str(get_fig(ori_str, dest_str)))
 def update_figure(nb, ori_str, dest_str):
     if (not ori_str) or (not dest_str):
-        return ""
+        return []
     else:
-        return get_fig(ori_str, dest_str)    
+        return get_fig(ori_str, dest_str)
 
 
 if __name__ == '__main__':
