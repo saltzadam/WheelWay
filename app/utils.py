23cede4b90ec400d41884a3171b9235d1190650d
Adam Saltz <saltz.adam@gmail.com>
2020-06-17 01:48:38 -0400

    at long last

--- app/utils.py
+++ app/utils.py
@@ -55,7 +55,15 @@ def fixed_route(rows):
     # add rows, flipping the two points if necessary. should be
     # (start point, end point, angle class)
     first_row = rows.pop(0)
-    relinked = [process_row(first_row)]
+    second_row = rows.pop(0)
+    if first_row[1] == second_row[0]:
+        relinked = [first_row, second_row]
+    elif first_row[0] == second_row[0]:
+        relinked = [(first_row[1],first_row[0],first_row[2]),second_row[0]]
+    elif first_row[1] == second_row[1]:
+        relinked = [first_row, (second_row[1], second_row[0], second_row[2])]
+    elif first_row[0] == second_row[1]:
+        relinked = [(first_row[1],first_row[0],first_row[2]), (second_row[1], second_row[0], second_row[2])]
     for row in rows:
         #last row:
         if row == (None, None, None):
@@ -83,7 +91,6 @@ def query_route(ori_int, des_int, routing, cur):
                        LEFT JOIN my_edges b 
                        ON (a.edge = b.osmid)""", (ori_int, des_int))
         raw_route = cur.fetchall()
-        print(raw_route)
         return fixed_route(raw_route), FOUND_ROUTE_MESSAGE
     elif routing == 'ADA':
         cur.execute("""SELECT st_astext(st_startpoint(b.geom)), st_astext(st_endpoint(b.geom)), b.angleclass
